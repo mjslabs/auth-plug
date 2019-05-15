9e0580f35ce4adaf3d13ca393a9293ad66f2bbb7
Matt Stofko <matt@mjslabs.com>
2019-05-15 16:29:08 -0700

    Moving LDAP mock to testlib

--- auth/ldap_test.go
+++ auth/ldap_test.go
@@ -1,56 +1,19 @@
 package auth
 
 import (
-	"errors"
 	"reflect"
 	"testing"
 
-	"github.com/c0sco/go-ldap-client"
-	"github.com/oleiade/reflections"
+	"github.com/mjslabs/auth-plug/internal/testlib"
 	"github.com/stretchr/testify/assert"
 )
-
-// MockLDAP lets us test LDAP code without an actual connection to a server
-type MockLDAP ldap.LDAPClient
-
-var testUserInfo = "testing"
-
-// Authenticate mocks the method of the same name from LDAPClient
-func (l MockLDAP) Authenticate(username string, password string) (bool, map[string]string, error) {
-	if username == "testy" && password == "tester" {
-		Cfg.Serv.Fields = populateFields("mapstructure", User{})
-		userData := map[string]string{}
-		for _, field := range Cfg.Serv.Fields {
-			userData[field] = field + testUserInfo
-		}
-		return true, userData, nil
-	} else if username == "gimmie" && password == "anerror" {
-		return false, map[string]string{}, errors.New("the server can't be found")
-	}
-	// go-ldap-client returns an error with this string on failed login
-	return false, map[string]string{}, errors.New("Invalid Credentials")
-}
-
-// Close mocks the method of the same name from LDAPClient
-func (l MockLDAP) Close() {
-}
-
-// Connect mocks the method of the same name from LDAPClient
-func (l MockLDAP) Connect() error {
-	return nil
-}
-
-// GetGroupsOfUser mocks the method of the same name from LDAPClient
-func (l MockLDAP) GetGroupsOfUser(username string) ([]string, error) {
-	return []string{}, nil
-}
 
 // init is a stand-in for Initialize()
 func init() {
 	Cfg = Configuration{
 		JWTMethod: "HS512",
 		Serv: ServerAttributes{
-			Conn: MockLDAP{},
+			Conn: testlib.MockLDAP{},
 		},
 	}
 }
@@ -59,8 +22,7 @@ func TestLDAP(t *testing.T) {
 	t.Run("Validate_errored", testErrored)
 	t.Run("Validate_failed", testFailed)
 	t.Run("Validate_success", testSuccess)
-	t.Run("Connect", testConnect)
-	t.Run("Initialize_success", testInitialize)
+	t.Run("populateFields", testPopulateFields)
 }
 
 func testErrored(t *testing.T) {
@@ -79,17 +41,35 @@ func testFailed(t *testing.T) {
 
 func testSuccess(t *testing.T) {
 	// Correct login
-	valid, user, _ := ValidateLogin("testy", "tester")
+	valid, _, err := ValidateLogin("testy", "tester")
+	assert.Equal(t, err, nil)
 	assert.Equal(t, valid, true)
-	r := reflect.TypeOf(user)
+}
+
+func testPopulateFields(t *testing.T) {
+	myTagName := "mapstructure"
+	myFields := populateFields(myTagName, User{})
+
+	// Independently verify that populateFields parsed the struct correctly
+	r := reflect.TypeOf(User{})
 	for i := 0; i < r.NumField(); i++ {
-		tag := r.Field(i).Tag.Get("mapstructure")
-		if data, _ := reflections.GetField(user, r.Field(i).Name); tag != "" && data != tag+testUserInfo {
-			t.Errorf("LDAP data not mapped into User structure properly: %s, %s", data, tag)
+		tag := r.Field(i).Tag.Get(myTagName)
+		if tag == "" {
+			continue
+		}
+		if inSlice := stringInSlice(tag, myFields); !inSlice {
+			t.Errorf(
+				"LDAP data not mapped into User struct properly: %s tag '%s' not in %s",
+				myTagName, tag, myFields)
 		}
 	}
 }
 
-func testConnect(t *testing.T) {
-	assert.NotPanics(t, func() { Cfg.Serv.Conn.Connect() })
+func stringInSlice(s string, list []string) bool {
+	for _, item := range list {
+		if item == s {
+			return true
+		}
+	}
+	return false
 }
--- internal/testlib/testlib.go
+++ internal/testlib/testlib.go
@@ -1,9 +1,11 @@
 package testlib
 
 import (
+	"errors"
 	"net/http/httptest"
 	"strings"
 
+	"github.com/c0sco/go-ldap-client"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -17,4 +19,41 @@ func EchoSetup(method, path, reader string) (c echo.Context, rec *httptest.Respo
 	rec = httptest.NewRecorder()
 	c = e.NewContext(req, rec)
 	return c, rec
+}
+
+// MockLDAP lets us test LDAP code without an actual connection to a server
+type MockLDAP ldap.LDAPClient
+
+// MockLDAPFields is for field mapping tests in ValidateLogin
+var MockLDAPFields = []string{"uid", "gecos"}
+
+// LDAPHostFail - when the LDAP host is set to this, Connect() will fail
+var LDAPHostFail = "fail.com"
+
+// Authenticate mocks the method of the same name from LDAPClient
+func (l MockLDAP) Authenticate(username string, password string) (bool, map[string]string, error) {
+	if username == "testy" && password == "tester" {
+		return true, map[string]string{}, nil
+	} else if username == "gimmie" && password == "anerror" {
+		return false, map[string]string{}, errors.New("the server can't be found")
+	}
+	// go-ldap-client returns an error with this string on failed login
+	return false, map[string]string{}, errors.New("Invalid Credentials")
+}
+
+// Close mocks the method of the same name from LDAPClient
+func (l MockLDAP) Close() {
+}
+
+// Connect mocks the method of the same name from LDAPClient
+func (l MockLDAP) Connect() error {
+	if l.Host == LDAPHostFail {
+		return errors.New("Connection to server failed")
+	}
+	return nil
+}
+
+// GetGroupsOfUser mocks the method of the same name from LDAPClient
+func (l MockLDAP) GetGroupsOfUser(username string) ([]string, error) {
+	return []string{}, nil
 }
